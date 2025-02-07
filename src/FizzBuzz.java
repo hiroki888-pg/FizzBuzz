e1e8085ed6f2226865eeacc52091c787db663993
HirokiMiyamoto <197619881+hiroki888-pg@users.noreply.github.com>
2025-02-07 22:37:08 +0900

    課題提出

---
+++ src/FizzBuzz.java
@@ -0,0 +1,24 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0 && i % 5 == 0) {
+				//3と5の両方で割り切れる場合、「FizzBuzz」と表示
+				System.out.println("FizzBuzz");
+			} else if(i % 3 == 0) {
+				//3で割り切れる場合、「Fizz」と表示
+				System.out.println("Fizz");
+			} else if(i % 5 == 0) {
+				//5で割り切れる場合、「Buzz」と表示
+				System.out.println("Buzz");
+			} else {
+				//3と5どちらとも割り切れない場合、数値をそのまま表示
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
